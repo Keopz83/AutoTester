420fd85fc098058dc175a574004a95bbf783b3b1
José Lima <joselima.engineering@gmail.com>
2018-09-05 07:21:37 +0200

    Version working for default types only, with annotations on productive code.

---
+++ AutoTester/TestAssembly.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SampleAssembly;
+using System.ComponentModel;
+using static AutoTester.Annotations;
+
+namespace AutoTester
+{
+
+    public class TestAssembly
+    {
+
+        public static void TestAll(string assemblyName) {
+
+            Assembly assembly = Assembly.ReflectionOnlyLoad("SampleAssembly");
+
+            foreach (Type type in assembly.GetTypes()) {
+
+                var runType = Type.GetType(type.AssemblyQualifiedName);
+                var typeInstance = Activator.CreateInstance(runType);
+
+                Console.WriteLine(type.Name);
+
+                foreach(MethodInfo methodInfo in (runType as TypeInfo).DeclaredMethods) {
+
+                    Console.WriteLine();
+                    PrintMethoSignature(methodInfo);
+
+                    Console.WriteLine();
+
+                    var passed = true;
+                    for(var testCase = 0; testCase < methodInfo.GetCustomAttributes().Count(); testCase++) {
+
+                        Console.WriteLine($"\t\tTest case {testCase}:");
+
+                        var testCaseValues = (methodInfo.GetCustomAttributes().ElementAt(testCase) as TestCaseValues);
+                        var testInputValues = testCaseValues.Inputs.ToArray();
+                        var expectedOutput = testCaseValues.Output;
+
+                        Console.WriteLine($"\t\t\tInput values: {PrintValues(testInputValues)}");
+                        Console.WriteLine($"\t\t\tOutput values: {PrintValues(expectedOutput)}");
+
+                        var result = methodInfo.Invoke(typeInstance, testInputValues);
+                        try {
+                            Assert.AreEqual(expectedOutput, result, "Unexpected output.");
+                            Console.WriteLine("\t\t\tTest passed.");
+                        }
+                        catch (Exception e) {
+                            Console.WriteLine(e.Message);
+                            passed = false;
+                        }
+                    }
+
+                    if (!passed) {
+                        Assert.Fail();
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        private static void PrintMethoSignature(MethodInfo methodInfo) {
+            var argumentTokensStr = methodInfo.GetParameters().Select(x => x.ParameterType.Name + ": " + x.Name);
+            var argumentsStr = string.Join(", ", argumentTokensStr);
+            Console.WriteLine($"\t{methodInfo.Name}({argumentsStr})");
+        }
+
+        private static string PrintValues(params object[] values) {
+            return string.Join(", ", values.Select(x => x.ToString()));
+        }
+    }
+}
---
+++ AutoTesterAnnotations/Annotations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTester {
+
+    public static class Annotations {
+
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+        public class TestCaseValues : Attribute {
+
+            public object[] Inputs { get; set; }
+
+            public object Output { get; set; }
+        }
+
+
+    }
+}
